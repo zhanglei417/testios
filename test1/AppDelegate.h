1ba6081ad3ff5ca1ac3495d9a0e4c09ed8caa25e
zhanglei417 <277405786@qq.com>
2017-03-22 12:54:03 +0800

    test

---
+++ test1/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  test1
+//
+//  Created by phoenixzhang on 22/03/2017.
+//  Copyright © 2017 phoenixzhang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ test1/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  RootViewController.h
+//  test1
+//
+//  Created by phoenixzhang on 22/03/2017.
+//  Copyright © 2017 phoenixzhang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RootViewController : UIViewController <UIPageViewControllerDelegate>
+
+@property (strong, nonatomic) UIPageViewController *pageViewController;
+
+@end
+
