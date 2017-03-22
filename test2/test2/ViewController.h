b494c88976f8172ee72156ca0a34eb6713a6410b
zhanglei417 <277405786@qq.com>
2017-03-22 12:59:17 +0800

    test

---
+++ test2/test2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  test2
+//
+//  Created by phoenixzhang on 22/03/2017.
+//  Copyright © 2017 phoenixzhang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ test2/test2/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  test2
+//
+//  Created by phoenixzhang on 22/03/2017.
+//  Copyright © 2017 phoenixzhang. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
